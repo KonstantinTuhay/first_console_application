3deca6d43667f15702903c6d7e25aff63010136f
Konstantin <tugay-k@mail.ru>
2023-11-28 22:04:32 +0300

    Third task for console application

--- js/script.js
+++ js/script.js
@@ -13,3 +13,15 @@ const personalMovieDB = {
   genres: [],
   privat: false,
 };
+
+// 3)
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+  b = prompt("На сколько оцените его?", ""),
+  c = prompt("Один из последних просмотренных фильмов?", ""),
+  d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB.movies);
