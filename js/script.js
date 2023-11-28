1304b3519c4959ba82fb7adc19d7921a018fa93a
Konstantin <tugay-k@mail.ru>
2023-11-28 22:02:12 +0300

    Second task for console application

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@
 //1)
 let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 console.log(numberOfFilms);
+
+// 2)
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
