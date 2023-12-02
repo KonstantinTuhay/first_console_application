528d88390571993d57766de27fe1eee7768a72ba
Konstantin <tugay-k@mail.ru>
2023-12-02 11:55:47 +0300

    Change my console apps

--- js/script.js
+++ js/script.js
@@ -16,12 +16,32 @@ const personalMovieDB = {
 
 // 3)
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  if (
+    a !== "" &&
+    a !== null &&
+    a.length < 50 &&
+    b !== "" &&
+    b !== null &&
+    b.length < 50
+  ) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
+}
+
+if (personalMovieDB.count <= 10) {
+  alert("Просмотрено мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+  alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  alert("Вы киноман");
+} else {
+  alert("Произошла ошибка");
+}
 
 console.log(personalMovieDB.movies);
